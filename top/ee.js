fa5479f0e294aa91f81ef6651733e7b2b21dfd30
dvishnyakov <210035@gmail.com>
2016-01-09 19:51:17 +0300

    Lesson09

---
+++ top/ee.js
@@ -0,0 +1,9 @@
+var EventEmitter = require('events').EventEmitter;
+
+var server = new EventEmitter;
+
+server.on('error', function(err) {
+    console.log(err);
+});
+
+server.emit('error', new Error('серверная ошибка'));
