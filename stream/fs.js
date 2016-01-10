ed8408d645ab879519bbf995b1ebf119003be553
dvishnyakov <210035@gmail.com>
2016-01-10 12:21:23 +0300

    Lesson21

---
+++ stream/fs.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+
+var stream = new fs.ReadStream('nono.html');
+
+stream.on('readable', function() {
+    var data = stream.read();
+    console.log(data.length);
+});
+
+stream.on('end', function() {
+    console.log('THE END');
+});
+
+stream.on('error', function(err) {
+    if (err.code == 'ENOENT') {
+        console.log('Файл не найден, попинайте админа, пусть выложит...')ж
+    } else {
+        console.error(err);
+    }
+});
