7b8077d7c9f9b5a4e65616d8ab2859bf2de3b739
dvishnyakov <210035@gmail.com>
2016-01-10 21:44:42 +0300

    Lesson31

--- chat/createDb.js
+++ chat/createDb.js
@@ -1,20 +1,14 @@
-var MongoClient = require('mongodb').MongoClient, format = require('util').format;
+var User = require('models/user').User;
 
-MongoClient.connect('mongodb://127.0.0.1:27017/chat', function(err, db) {
-    if(err) throw err;
+var user = new User({
+    username: 'Tester2',
+    password: 'sercret'
+});
 
-    var collection = db.collection('test_insert');
-    collection.remove({}, function(err, affected) {
-        if (err) throw err;
+user.save(function(err, user, affected) {
+    if (err) throw err;
 
-        collection.insert({a: 2}, function(err, docs) {
-
-            var cursor = collection.find({a: 2});
-            cursor.toArray(function(err, results) {
-                console.dir(results);
-                db.close();
-            });
-
-        });
+    User.findOne({username: 'Tester'}, function(err, tester) {
+        console.log(tester);
     });
 });
